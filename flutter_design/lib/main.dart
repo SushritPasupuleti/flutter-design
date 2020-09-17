1f758a1950f6100c01d94c4412485504f69edb96
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-09-17 11:08:57 +0530

    add dummy bottom nav bar

--- flutter_design/lib/main.dart
+++ flutter_design/lib/main.dart
@@ -82,6 +82,23 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               body: FABDisplay(),
               //floatingActionButton: 
+              bottomNavigationBar: BottomNavigationBar(
+       currentIndex: 0, // this will be set when a new tab is tapped
+       items: [
+         BottomNavigationBarItem(
+           icon: new Icon(Icons.home),
+           title: new Text('Home'),
+         ),
+         BottomNavigationBarItem(
+           icon: new Icon(Icons.mail),
+           title: new Text('Messages'),
+         ),
+         BottomNavigationBarItem(
+           icon: Icon(Icons.person),
+           title: Text('Profile')
+         )
+       ],
+     ),
             );
           
   }
